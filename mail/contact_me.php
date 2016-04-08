209b2530d8093a9cb4dfc8ec12b9e6628cb8e00a
khalilsomani <somanik@uw.edu>
2016-04-08 12:26:19 -0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,4 @@
 <?php
-
-require_once "Mail.php";
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -11,73 +9,18 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-
-$from = '<noreply@khalilsomani.com>'; //change this to your email address
-$to = '<khalilsomani@gmail.com>'; // change to address
-$subject = "Website Contact Form:  $name"; // subject of mail
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-
-
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
-}
-	}
-
-/* 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-
-$from = '<noreply@khalilsomani.com>'; //change this to your email address
-$to = '<khalilsomani@gmail.com>'; // change to address
-$subject = "Website Contact Form:  $name"; // subject of mail
-$message = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-
-$host = "ssl://smtp.gmail.com";
-$port = "465";
-$username = "noreply@khalilsomani.com";
-$password = "lyvdugllndrbiquf";
-
-$headers = array ('From' => $from,
-  'To' => $to,
-  'Subject' => $subject);
-$smtp = Mail::factory('smtp',
-  array ('host' => $host,
-    'port' => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-  echo("<p>" . $mail->getMessage() . "</p>");
-} else {
-  echo("<p>Message successfully sent!</p>");
-}
-
-*/
 	
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-//$to = 'khalilsomani@gmail.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-//$email_subject = "Website Contact Form:  $name";
-//$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-//$headers = "From: noreply@khalilsomani.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-//$headers .= "Reply-To: $email_address";	
-//mail($to,$email_subject,$email_body,$headers); 
+$to = 'khalilsomani@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@khalilsomani.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
